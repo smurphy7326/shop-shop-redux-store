669571759e44028818acee86ae03c544a668dd4d
smurphy7326 <smurphy7326@gmail.com>
2021-12-22 21:11:37 -0500

    had to change the app.js and had to move things around still working on it I think

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,10 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+import { Provider } from 'react-redux';
+
+import store from './utils/store';
+
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
@@ -42,7 +45,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider client={client}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +56,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
